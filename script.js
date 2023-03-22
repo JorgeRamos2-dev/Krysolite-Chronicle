e8f54966703b30fe6bee2e1535b916c8cb8ff45d
JorgeRamos2-dev <jorgeramos201003@gmail.com>
2023-03-21 22:31:50 -0600

    Primer Boceto del blog de videojuegos.

---
+++ script.js
@@ -0,0 +1,10 @@
+function openSidebar() {
+    document.getElementById("sidebarMenu").style.width = "250px";
+    document.getElementById("mainContent").style.marginLeft = "250px";
+  }
+  
+  function closeSidebar() {
+    document.getElementById("sidebarMenu").style.width = "0";
+    document.getElementById("mainContent").style.marginLeft = "0";
+  }
+  
