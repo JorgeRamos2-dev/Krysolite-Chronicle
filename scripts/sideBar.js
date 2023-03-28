687921b9333702a45563a2e19c1410c2e2140a87
kyomuapashi <kyomuapashi@gmail.com>
2023-03-28 12:44:00 -0600

    Reemplazo de menú desplegable

--- scripts/sideBar.js
+++ scripts/sideBar.js
@@ -1,10 +1,6 @@
-function openSidebar() {
-    document.getElementById("sidebarMenu").style.width = "250px";
-    document.getElementById("mainContent").style.marginLeft = "250px";
-  }
-  
-  function closeSidebar() {
-    document.getElementById("sidebarMenu").style.width = "0";
-    document.getElementById("mainContent").style.marginLeft = "0";
-  }
-  
+function toggleMenu() {
+  const sidebar = document.querySelector(".sidebar");
+  const menuToggle = document.querySelector(".menu-toggle");
+  sidebar.classList.toggle("active");
+  menuToggle.classList.toggle("active");
+}
