43f9c587c7416de58cf57a229b26b0c6b8b22b80
kyomuapashi <kyomuapashi@gmail.com>
2023-04-25 13:31:27 -0600

    Estructura del sistema de archivos y pagina contacto
    Se reestructuro el sistema de archivos para que fuese consistente con el sistema de bootstrap, y se modificó la página de contacto

---
+++ js/sideBar.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+  const sidebar = document.querySelector(".sidebar");
+  const menuToggle = document.querySelector(".menu-toggle");
+  sidebar.classList.toggle("active");
+  menuToggle.classList.toggle("active");
+}
+
