b9055ac0a6788ee18143777f0cf5825ac771c331
RogerG03 <fernando.rogerguido@gmail.com>
2023-04-27 12:15:36 -0600

    Rediseño completo de la pagina
    Se ha decidido adoptar por completo los diseños de bootstrap para realizar la página, por lo que se han rediseñado las paginas por completo

--- js/sideBar.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-  const sidebar = document.querySelector(".sidebar");
-  const menuToggle = document.querySelector(".menu-toggle");
-  sidebar.classList.toggle("active");
-  menuToggle.classList.toggle("active");
-}
-
